1d960f8d7b6c9e7042cecade18363935cd23238a
mohitsvpi <mohit.singhal@masaischool.com>
2022-12-20 17:00:08 +0530

    Checking google oauth issues

--- src/configs/google-oauth.js
+++ src/configs/google-oauth.js
@@ -9,7 +9,7 @@ passport.use(
       {
          clientID: process.env.GOOGLE_CLIENT_ID,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-         callbackURL: "http://localhost:2345/auth/google/callback",
+         callbackURL: "https://beta-project-x.cyclic.app/auth/google/callback",
          passReqToCallback: true,
       },
       async function (request, accessToken, refreshToken, profile, done) {
